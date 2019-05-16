3ae69d6f91f7971265c3c0b6f412e064b5cc9793
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-16 18:38:41 +0200

    ok

--- packages/terminal/src/Term.tsx
+++ packages/terminal/src/Term.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import axios from 'axios';
 import { Terminal } from 'xterm';
 import { AttachAddon } from 'xterm-addon-attach';
+import * as fullscreen from 'xterm/lib/addons/fullscreen/fullscreen';
 import 'xterm/dist/xterm.css';
+import 'xterm/dist/addons/fullscreen/fullscreen.css';
+
+Terminal.applyAddon(fullscreen);
 
 function runRealTerminal(term: Terminal, socket: WebSocket): void {
     term.loadAddon(new AttachAddon(socket));
@@ -12,9 +16,13 @@ const setContainer = async (container: HTMLDivElement) => {
     if (container) {
         console.log('Load terminal container');
         const windowsMode = ['Windows', 'Win16', 'Win32', 'WinCE'].indexOf(navigator.platform) >= 0;
-        const term = new Terminal({ windowsMode });
+        const term = new Terminal({
+            windowsMode,
+            // cols: 80,
+        });
         (window as any).term = term;  // Expose `term` to window for debugging purposes
         term.open(container);
+        (term as any).toggleFullScreen(true);
         term.focus();
 
         const { data: pid } = await axios.post(`/terminals?cols=${term.cols}&rows=${term.rows}`, {});
